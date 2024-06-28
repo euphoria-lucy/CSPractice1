a3935c13325494cf262a2d623a1eab78f97b3a32
Misung Yang <voielactee7@naver.com>
2024-06-28 11:06:31 +0900

    #4 21-3. 클래스 생성

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -6,8 +6,18 @@ using System.Threading.Tasks;
 
 namespace CSPractice1
 {
+    class FirstClass
+    {
+
+    }
+
     internal class Program
     {
+        class ThirdClass
+        {
+
+        }
+
         static void Main(string[] args)
         {
             // #1 12-1. 기본용어
@@ -165,6 +175,9 @@ namespace CSPractice1
             Console.WriteLine(Math.Min(42, 1)); // 1
             Console.WriteLine(Math.Max(42, 1)); // 42
             Console.WriteLine(Math.PI); // 3.141562....
+
+            // #4 21-3. 클래스 생성
+
         }
     }
 }
