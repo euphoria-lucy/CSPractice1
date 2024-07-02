e690d529890786e42958c4ceb8c90d5bfc8feb9c
Misung Yang <voielactee7@naver.com>
2024-07-02 14:04:36 +0900

    #5 22-7. 소멸자

--- CSPractice1/Program.cs
+++ CSPractice1/Program.cs
@@ -18,6 +18,10 @@ namespace CSPractice1
         {
             Console.WriteLine("(인스턴스) 생성자 호출");
         }
+        ~Sample()
+        {
+            Console.WriteLine("소멸자 호출");
+        }
     }
      
     class MyMath
@@ -250,6 +254,8 @@ namespace CSPractice1
             Console.WriteLine("두 번째 위치");
             Console.WriteLine(Sample.value);
             Console.WriteLine("세 번째 위치");
+
+            // #5 22-7. 소멸자
         }
     }
 }
